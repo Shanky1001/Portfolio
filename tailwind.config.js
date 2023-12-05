b82c29e494eb2853ca8f5cc87821c9b3491159df
Shanky1001 <sr1071995@gmail.com>
2023-12-05 13:26:51 +0530

    added dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        400: "400px",
+        500: "500px",
+        760: "760px",
+        900: "900px",
+        1000: "1000px",
+        1100: "1100px",
+        1200: "1200px",
+        1600: "1600px",
+        1300: "1300px",
+      },
+    },
+  },
+  plugins: [],
+};
