e5e8b83359cf05c41a47a8ae6d4a762407ece231
Shashank Rai <108118506+Shanky1001@users.noreply.github.com>
2024-11-09 23:42:23 +0530

    fixed cursor in case of window rescaling
    - fixed the cursor circle position during window rescaling

--- src/component/cursor/CustomCursor.tsx
+++ src/component/cursor/CustomCursor.tsx
@@ -19,10 +19,21 @@ const CustomCursor = () => {
       //     setPosition({ x: e.clientX , y: e.clientY  });
       //   }, 1000);
     };
+    // Scale handling for window resizing
+  const handleResize = () => {
+    if (cursorRef.current) {
+      const { innerWidth, innerHeight } = window;
+      setPosition((prev) => ({
+        x: (prev.x / innerWidth) * window.innerWidth,
+        y: (prev.y / innerHeight) * window.innerHeight,
+      }));
+    }
+  };
     window.addEventListener("mousemove", handleMouseMove);
-
+    window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -30,6 +41,7 @@ const CustomCursor = () => {
     <motion.div
       className="cursor fixed w-3 h-3 rounded-full border pointer-events-none bg-black dark:bg-white z-50"
       animate={{ x: position.x, y: position.y }}
+      transition={{ type: "spring", stiffness: 300, damping: 20 }}
     ></motion.div>
   );
 };
