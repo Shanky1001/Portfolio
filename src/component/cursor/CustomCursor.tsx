90ab5913b577511e35645890b2339742c170f0c4
Shanky1001 <sr1071995@gmail.com>
2024-11-10 00:54:37 +0530

    fixed cursor

--- src/component/cursor/CustomCursor.tsx
+++ src/component/cursor/CustomCursor.tsx
@@ -1,45 +1,32 @@
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
+const maxWidth = 1600;
 const CustomCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const cursorRef = useRef(null);
-  //   // Debounce function to limit the rate of execution
-  //   const debounce = (func, delay:number) => {
-  //     clearTimeout(timeoutId);
-  //     timeoutId = setTimeout(func, delay);
-  //   };
 
   useEffect(() => {
     const handleMouseMove = (e: { clientX: number; clientY: number }) => {
-      setPosition({ x: e.clientX - 5, y: e.clientY - 3 });
-      //   debounce(() => {
-      //     setPosition({ x: e.clientX , y: e.clientY  });
-      //   }, 1000);
+      const xPercent = (e.clientX / window.innerWidth) * 100;
+      const yPercent = (e.clientY / window.innerHeight) * 100;
+      setPosition({ x: xPercent, y: yPercent });
     };
-    // Scale handling for window resizing
-  const handleResize = () => {
-    if (cursorRef.current) {
-      const { innerWidth, innerHeight } = window;
-      setPosition((prev) => ({
-        x: (prev.x / innerWidth) * window.innerWidth,
-        y: (prev.y / innerHeight) * window.innerHeight,
-      }));
-    }
-  };
     window.addEventListener("mousemove", handleMouseMove);
-    window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
-      window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  // Calculate the pixel position for x and y
+  const xPos = (position.x / 100) * Math.min(window.innerWidth, maxWidth);
+  const yPos = (position.y / 100) * window.innerHeight;
 
   return (
     <motion.div
       ref={cursorRef}
-      className="cursor fixed w-3 h-3 rounded-full border pointer-events-none bg-black dark:bg-white z-50"
-      animate={{ x: position.x, y: position.y }}
+      className="cursor fixed w-3 h-3 rounded-full border origin-center pointer-events-none bg-black dark:bg-white z-50"
+      animate={{ x: xPos, y: yPos }}
       transition={{ type: "spring", stiffness: 300, damping: 20 }}
     ></motion.div>
   );
