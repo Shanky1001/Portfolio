90abb7c4db12e96b05d7c011f868358c833b2687
Shashank Rai <108118506+Shanky1001@users.noreply.github.com>
2024-11-15 16:25:45 +0530

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,7 +5,7 @@ import { getAnalytics, logEvent, setUserProperties } from "firebase/analytics";
 const {API_KEY,REACT_APP_API_KEY,REACT_APP_AUTH_DOMAIN,REACT_APP_DATABASE_URL,REACT_APP_PROJECT_ID,REACT_APP_STORAGE_BUCKET,REACT_APP_MESSAGING_SENDER_ID,REACT_APP_APP_ID} = process.env
  // Firebase config
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
