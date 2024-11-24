985de0fe4e804b1772028cadfb0fca15576dff01
shanky1001 <sr1071995@gmail.com>
2024-11-24 23:15:46 +0530

    trying to fix analytics error in prod

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,6 @@
-import { getDatabase } from "firebase/database";
-import { initializeApp } from "firebase/app";
-import { getAnalytics, logEvent, setUserProperties } from "firebase/analytics";
+import { getDatabase } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, logEvent, setUserProperties } from 'firebase/analytics';
 
 // Firebase config
 const firebaseConfig = {
@@ -17,6 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics();
 
 export { db, analytics, logEvent, setUserProperties };
